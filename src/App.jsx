765ccbbfa9831a7a1b7530c0f6ead08c46b5cfe4
Sudalai02 <dev.sudalai@gmail.com>
2025-08-29 07:18:48 -0700

    User-management projects

---
+++ src/App.jsx
@@ -0,0 +1,174 @@
+import { useEffect, useRef, useState } from 'react';
+import {
+  Button,
+  EditableText,
+  InputGroup,
+  OverlayToaster,
+  Position
+} from '@blueprintjs/core';
+import './App.css';
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [newWebsite, setNewWebsite] = useState("");
+
+  const toasterRef=useRef(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((json) => setUsers(json));
+  }, []);
+
+  function addUser() {
+    const name = newName.trim();
+    const email = newEmail.trim();
+    const website = newWebsite.trim();
+
+    if (name && email && website) {
+      fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "POST",
+        body: JSON.stringify({ name, email, website }),
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8"
+        }
+      })
+        .then((response) => response.json())
+        .then((data) => {
+
+          const newUser={...data, id:users.length+1 }
+          setUsers([...users, newUser]);
+
+          toasterRef.current?.show({
+            message: "User added successfully",
+            intent: 'success',
+            timeout: 3000,
+          });
+          setNewName("");
+          setNewEmail("");
+          setNewWebsite("");
+        });
+    }
+  }
+
+  function onChangeHandler(id, key, value) {
+    setUsers((users) =>
+      users.map(user =>
+        user.id === id ? { ...user, [key]: value } : user
+      )
+    );
+  }
+
+  function updateUser(id) {
+    const user = users.find((u) => u.id === id);
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8"
+      }
+    })
+      .then((response) => response.json())
+      .then(() => {
+        toasterRef.current?.show({
+          message: "User updated successfully",
+          intent: 'success',
+          timeout: 3000
+        });
+      });
+  }
+
+  function deleteUser(id) {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => {
+        setUsers((users) => users.filter(user => user.id !== id));
+        toasterRef.current?.show({
+          message: "User deleted successfully",
+          intent: 'success',
+          timeout: 3000,
+        });
+      });
+  }
+
+  return (
+    <div className="App">
+      <table className='bp4-html-table bp4-html-table-bordered bp4-html-table-striped'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Website</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user =>
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>
+                <EditableText
+                  onChange={(value) => onChangeHandler(user.id, 'email', value)}
+                  value={user.email}
+                />
+              </td>
+              <td>
+                <EditableText
+                  onChange={(value) => onChangeHandler(user.id, 'website', value)}
+                  value={user.website}
+                />
+              </td>
+              <td>
+                <Button intent='primary' onClick={() => updateUser(user.id)}>Update</Button>
+                &nbsp;
+                <Button intent='danger' onClick={() => deleteUser(user.id)}>Delete</Button>
+              </td>
+            </tr>
+          )}
+
+        </tbody>
+
+
+        <tfoot>
+          <tr>
+            <td></td>
+            <td>
+              <InputGroup
+                value={newName}
+                onChange={(e) => setNewName(e.target.value)}
+                placeholder='Enter Name...'
+              />
+            </td>
+            <td>
+              <InputGroup
+                value={newEmail}
+                onChange={(e) => setNewEmail(e.target.value)}
+                placeholder='Enter Email...'
+              />
+            </td>
+            <td>
+              <InputGroup
+                value={newWebsite}
+                onChange={(e) => setNewWebsite(e.target.value)}
+                placeholder='Enter Website...'
+              />
+            </td>
+            <td>
+              <Button intent='success' onClick={addUser}>Add User</Button>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+
+      <OverlayToaster position={Position.TOP} ref={toasterRef} />
+    </div>
+  );
+}
+
+export default App;
