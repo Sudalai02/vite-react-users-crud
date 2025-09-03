0b50aafa19411a44159ebc946a9550f43d90dffc
Sudalai02 <dev.sudalai@gmail.com>
2025-09-02 20:16:34 -0700

    CRUD Projects

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://sudalai02.github.io/vite-react-users-crud/",
   plugins: [react()],
 })
